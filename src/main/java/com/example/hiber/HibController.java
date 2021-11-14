80025eff51d953511b7f0397f23dd1e32fb4a078
ruagegd <elena.kosheleva@raiffeisen.ru>
2021-11-14 21:38:21 +0300

    Method security

--- src/main/java/com/example/hiber/HibController.java
+++ src/main/java/com/example/hiber/HibController.java
@@ -1,10 +1,14 @@
 package com.example.hiber;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,26 +23,32 @@ public class HibController {
     }
 
     @GetMapping("/h")
-    public String getHello(){
+    public String getHello() {
         return "Hello";
     }
 
+
     @GetMapping("/by-city")
+    @Secured("ROLE_READ")
     public List<Persons> getPersonsByCity(@RequestParam String city) {
         return repository.getByCityOfLiving(city);
     }
 
+
     @GetMapping("/age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Persons> getPersonsByCity(@RequestParam int age) {
         return repository.findByPersonsPKAgeLessThanOrderByPersonsPKAge(age);
     }
 
     @GetMapping("/name")
+    @PreAuthorize("#name == authentication.principal.username")
     public Optional<Persons> getPersonsByCity(@RequestParam String name, @RequestParam String surname) {
-        return repository.getPersonsByPersonsPKNameAndAndPersonsPKSurname(name,surname);
+        return repository.getPersonsByPersonsPKNameAndAndPersonsPKSurname(name, surname);
     }
 
     @GetMapping("/")
+    @PostAuthorize("hasRole('ROLE_DELETE') or hasRole('ROLE_WRITE')")
     public String preparation() {
         repository.save(new Persons(
                 "ilya", "Li", 35, "77777777", "Ryazan"));
--- src/main/java/com/example/hiber/SecurityConfig.java
+++ src/main/java/com/example/hiber/SecurityConfig.java
@@ -2,23 +2,19 @@ package com.example.hiber;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("user1").password("{noop}pass").roles("ALL");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http)throws Exception{
-        http.formLogin().and()
-            .authorizeRequests().antMatchers("/persons/h").permitAll()
-            .and()
-            .authorizeRequests().anyRequest().authenticated();
+        auth.inMemoryAuthentication().withUser("Petr").password("{noop}pass").roles("READ");
+        auth.inMemoryAuthentication().withUser("user3").password("{noop}pass").roles("WRITE");
+        auth.inMemoryAuthentication().withUser("user4").password("{noop}pass").roles("DELETE");
     }
 
 }
